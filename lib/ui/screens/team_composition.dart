621a908c16a624eb37f0bcc921a4a13e1f6db9eb
Julien A <julien.arcioni@gmail.com>
2024-10-14 14:25:58 +0200

    Join team

--- lib/ui/screens/team_composition.dart
+++ lib/ui/screens/team_composition.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class TeamCompositionScreen extends StatelessWidget {
-  final String sessionId; // Ajout de l'ID de session comme paramètre
+class TeamCompositionScreen extends StatefulWidget {
+  final String sessionId;
 
   const TeamCompositionScreen({Key? key, required this.sessionId}) : super(key: key);
+
+  @override
+  _TeamCompositionScreenState createState() => _TeamCompositionScreenState();
+}
+
+class _TeamCompositionScreenState extends State<TeamCompositionScreen> {
+  List<String> blueTeam = [];
+  List<String> redTeam = [];
+  String? errorMessage;
 
   @override
   Widget build(BuildContext context) {
@@ -30,23 +42,39 @@ class TeamCompositionScreen extends StatelessWidget {
               style: Theme.of(context).textTheme.headlineSmall,
             ),
             const SizedBox(height: 10),
-            _buildTeamTile(context, color: Colors.blue),
+            _buildTeamTile(blueTeam, Colors.blue),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _joinTeam('blue'),
+              child: const Text('Rejoindre Équipe Bleue'),
+            ),
             const SizedBox(height: 40),
             Text(
               'Equipe Rouge',
               style: Theme.of(context).textTheme.headlineSmall,
             ),
             const SizedBox(height: 10),
-            _buildTeamTile(context, color: Colors.red),
+            _buildTeamTile(redTeam, Colors.red),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _joinTeam('red'),
+              child: const Text('Rejoindre Équipe Rouge'),
+            ),
             const Spacer(),
             ElevatedButton.icon(
               onPressed: () {
-                _showQRCodeDialog(context, sessionId); // Appel à la méthode pour afficher le QR code
+                _showQRCodeDialog(context, widget.sessionId);
               },
               icon: const Icon(Icons.qr_code),
               label: const Text('Inviter des amis'),
             ),
             const SizedBox(height: 20),
+            errorMessage != null
+                ? Text(
+              errorMessage!,
+              style: const TextStyle(color: Colors.red),
+            )
+                : Container(),
             Text(
               'La partie sera lancée automatiquement une fois les joueurs au complet',
               style: Theme.of(context).textTheme.bodySmall,
@@ -59,7 +87,7 @@ class TeamCompositionScreen extends StatelessWidget {
   }
 
   // Widget pour construire la tuile d'équipe
-  Widget _buildTeamTile(BuildContext context, {required Color color}) {
+  Widget _buildTeamTile(List<String> team, Color color) {
     return Container(
       padding: const EdgeInsets.all(16.0),
       decoration: BoxDecoration(
@@ -75,23 +103,53 @@ class TeamCompositionScreen extends StatelessWidget {
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '<en attente>',
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: Colors.white,
-            ),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            '<en attente>',
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: Colors.white,
-            ),
-          ),
-        ],
+        children: team.isNotEmpty
+            ? team.map((player) => Text(player, style: Theme.of(context).textTheme.bodyLarge!.copyWith(color: Colors.white))).toList()
+            : [Text('<Aucun joueur>', style: Theme.of(context).textTheme.bodyLarge!.copyWith(color: Colors.white))],
       ),
     );
+  }
+
+  // Fonction pour rejoindre une équipe
+  Future<void> _joinTeam(String color) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? jwt = prefs.getString('jwt');
+    String userName = prefs.getString('userName') ?? 'Joueur';
+
+    if (jwt == null) {
+      setState(() {
+        errorMessage = 'Erreur : Utilisateur non authentifié';
+      });
+      return;
+    }
+
+    final url = Uri.parse('https://pictioniary.wevox.cloud/api/game_sessions/${widget.sessionId}/join');
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $jwt',
+      },
+      body: jsonEncode({'color': color}),
+    );
+
+    if (response.statusCode == 200) {
+      print('Objet GameSession retourné : ${response.body}');
+      setState(() {
+        if (color == 'blue') {
+          blueTeam.add(userName);
+          print('utilisateur ajouté à la blueTeam');
+        } else {
+          redTeam.add(userName);
+          print('utilisateur ajouté à la redTeam');
+        }
+        errorMessage = null; // Réinitialiser le message d'erreur
+      });
+    } else {
+      setState(() {
+        errorMessage = 'Erreur lors de la jonction à l\'équipe';
+      });
+    }
   }
 
   // Méthode pour afficher le QR code dans un Dialog
